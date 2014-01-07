5a2ca860a1bde4417c4e7da48ebbab7247a42914
acompagno <acompag@gmail.com>
2014-01-07 09:11:40 -0500

    Get rid of warnings

--- AndroidExample/src/com/acompagno/example/MainActivity.java
+++ AndroidExample/src/com/acompagno/example/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
